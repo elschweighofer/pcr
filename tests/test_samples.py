f2de75cb9fb82d9cbbb12157b8a0fbecb37982ac
Lukas Schweighofer <lukas.schweighofer@outlook.at>
2022-10-30 12:58:04 +0100

    changed samples testfile

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -3,5 +3,6 @@ import unittest
 from app.main import post_sample
 from app.models.sample import Sample
 
-def test_post_sample():
-    self.assertEqual(response, Sample)
+def test_post_sample(self):
+    
+    self.assertEqual()
