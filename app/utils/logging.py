28b3be89a0a37757f62fa397c9dcf39113229a84
Lukas Schweighofer <lukas.schweighofer@outlook.at>
2022-11-03 20:41:11 +0100

    added logger

---
+++ app/utils/logging.py
@@ -0,0 +1,13 @@
+import logging
+import sys
+
+
+logger = logging.getLogger()
+fileHandler = logging.FileHandler("logfile.log")
+streamHandler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+fileHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+logger.addHandler(fileHandler)
+logger.setLevel("DEBUG")
